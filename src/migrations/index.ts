082eb5020efee9e7e450d24ee277742f3e841e8c
Xnths <jonathascastilhoes@gmail.com>
2026-01-25 00:50:48 -0300

    feat: adding migrations

---
+++ src/migrations/index.ts
@@ -0,0 +1,9 @@
+import * as migration_20260125_034751 from './20260125_034751';
+
+export const migrations = [
+  {
+    up: migration_20260125_034751.up,
+    down: migration_20260125_034751.down,
+    name: '20260125_034751'
+  },
+];
